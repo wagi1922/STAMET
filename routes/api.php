041442626c3a104d8d449ef60f035e6dd845df7c
wagi1922 <wagiartono1922@gmail.com>
2025-08-10 13:12:30 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -8,35 +8,6 @@ use App\Http\Middleware\ApiKeyMiddleware;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PengajuanMagangController;
 
-/*
-|--------------------------------------------------------------------------
-| RUTE DEBUG SEMENTARA (DIPERBAIKI)
-|--------------------------------------------------------------------------
-|
-| Rute ini sekarang mencari file dengan key 'path_dokumen'.
-|
-*/
-Route::post('/test-upload', function (Request $request) {
-    // PERUBAHAN DI SINI: Mencari file dengan key 'path_dokumen'
-    $hasFile = $request->hasFile('path_dokumen');
-    $isFileValid = $request->file('path_dokumen') ? $request->file('path_dokumen')->isValid() : false;
-
-    return response()->json([
-        'Pesan' => 'Hasil Debug Upload (Diperbaiki)',
-        'Key yang dicari' => 'path_dokumen',
-        'Apakah ada file?' => $hasFile,
-        'Apakah file valid?' => $isFileValid,
-        'Detail File' => $request->file('path_dokumen'),
-        'Semua Input' => $request->all()
-    ]);
-});
-
-
-/*
-|--------------------------------------------------------------------------
-| RUTE APLIKASI ANDA
-|--------------------------------------------------------------------------
-*/
 
 Route::middleware([ApiKeyMiddleware::class])->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
